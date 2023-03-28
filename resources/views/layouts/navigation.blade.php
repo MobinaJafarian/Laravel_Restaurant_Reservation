76ce0b58d9d835dd5ed4861af75fac9b627cbd8d
Mobina <nimobina99@gmail.com>
2023-03-28 15:37:58 +0330

    add AdminRoute

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    @if(Auth::user()->is_admin)
+                    <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                        {{ __('Admin') }}
                     </x-nav-link>
+                    @endif
                 </div>
             </div>
 
