ac18ba167def66e7f5c0d4392244b5cc7a0ff3d5
Mobina <nimobina99@gmail.com>
2023-03-28 15:48:52 +0330

    add reservation model & migration

---
+++ database/migrations/2023_03_28_121132_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('tel_number');
+            $table->dateTime('res_date');
+            $table->unsignedBigInteger('table_id');
+            $table->integer('guest_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
