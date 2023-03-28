2e59d83ee5b686fa2321ba5076c64c6c64dd0e68
Mobina <nimobina99@gmail.com>
2023-03-28 16:23:38 +0330

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -32,6 +36,10 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth' , 'admin'])->name('admin.')->prefix('admin')->group(function() {
     Route::get('/' , [AdminController::class , 'index'])->name('index');
+    Route::resource('/categories' , CategoryController::class);
+    Route::resource('/menu' , MenuController::class);
+    Route::resource('/tables' , TableController::class);
+    Route::resource('/reservation' , ReservationController::class);
 });
 
 require __DIR__.'/auth.php';
