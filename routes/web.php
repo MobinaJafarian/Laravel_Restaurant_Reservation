8752555461d16e559f6067a6b93cba92208ab53c
Mobina <nimobina99@gmail.com>
2023-03-28 15:19:51 +0330

    add Admin route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware(['auth' , 'admin'])->name('admin.')->prefix('admin')->group(function() {
+    Route::get('/' , [AdminController::class , 'index'])->name('index');
 });
 
 require __DIR__.'/auth.php';
