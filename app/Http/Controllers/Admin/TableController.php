7dd55a8b77948a1804f7b0b0c663ad45256fad24
Mobina <nimobina99@gmail.com>
2023-03-29 09:18:47 +0330

    add table controller & blade

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Table;
 use Illuminate\Http\Request;
 
 class TableController extends Controller
@@ -12,7 +13,8 @@ class TableController extends Controller
      */
     public function index()
     {
-        //
+        $tables = Table::all();
+        return view('admin.tables.index' , compact('tables'));
     }
 
     /**
