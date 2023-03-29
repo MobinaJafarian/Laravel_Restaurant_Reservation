d99441dc4adcd17c0c2306df787b5b9446106c0b
Mobina <nimobina99@gmail.com>
2023-03-29 09:18:30 +0330

    add reservation controller & blade

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -12,7 +13,8 @@ class ReservationController extends Controller
      */
     public function index()
     {
-        //
+        $reservations = Reservation::all();
+        return view('admin.reservations.index' , compact('reservations'));
     }
 
     /**
