8a1e8f21f00c02ad6b24d986183da3ca8aab67ab
mobinajafarian <nimobina99@gmail.com>
2023-04-12 03:27:30 +0330

    add store method

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -28,9 +29,17 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        //
+        $image = $request->file('image')->store('public/categories');
+
+        Category::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $image
+        ]);
+
+        return to_route('admin.categories.index')->with('success', 'Category created successfully.');
     }
 
     /**
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -11,42 +11,35 @@
                 <a href="{{ route('admin.categories.index') }}"
                     class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Category Index</a>
             </div>
-            <div class="m-2 p-2 bg-slate-100 rounded">
+            <div class="dark:bg-gray-700  m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form method="POST" action="{{ route('admin.categories.update', $category->id) }}"
-                        enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
                         @csrf
-                        @method('PUT')
                         <div class="sm:col-span-6">
-                            <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
+                            <label for="name" class="block text-sm font-medium text-gray-900"> Name </label>
                             <div class="mt-1">
-                                <input type="text" id="name" name="name" value="{{ $category->name }}"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="text" id="name" name="name"
+                                    class="block w-full appearance-none dark:bg-gray-400 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
                             </div>
                             @error('name')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="sm:col-span-6">
-                            <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
-                            <div>
-                                <img class="w-32 h-32" src="{{ Storage::url($category->image) }}">
-                            </div>
+                            <label for="image" class="block text-sm font-medium text-gray-900"> Image </label>
                             <div class="mt-1">
                                 <input type="file" id="image" name="image"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    class="block w-full appearance-none dark:bg-gray-400 border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
                             </div>
                             @error('image')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="sm:col-span-6 pt-5">
-                            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+                            <label for="description" class="block text-sm font-medium text-gray-900">Description</label>
                             <div class="mt-1">
                                 <textarea id="description" rows="3" name="description"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
-                                 {{ $category->description }}
-                                </textarea>
+                                    class="shadow-sm focus:ring-indigo-500 appearance-none dark:bg-gray-400 border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') border-red-400 @enderror"></textarea>
                             </div>
                             @error('description')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
@@ -54,7 +47,7 @@
                         </div>
                         <div class="mt-6 p-4">
                             <button type="submit"
-                                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Update</button>
+                                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
                         </div>
                     </form>
                 </div>
