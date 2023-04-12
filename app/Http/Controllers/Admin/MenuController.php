ac5081a1a7d85e87eaffffb8786727390b00fcb9
mobinajafarian <nimobina99@gmail.com>
2023-04-13 01:19:00 +0330

    update menu

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Menu;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class MenuController extends Controller
     public function index()
     {
         $menus = Menu::all();
-        return view('admin.menus.index' , compact('menus'));
+        return view('admin.menus.index', compact('menus'));
     }
 
     /**
@@ -22,7 +23,8 @@ class MenuController extends Controller
      */
     public function create()
     {
-        return view('admin.menus.index');
+        $categories = Category::all();
+        return view('admin.menus.create', compact('categories'));
     }
 
     /**
