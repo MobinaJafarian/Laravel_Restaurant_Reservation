dea100f45c6ab156164a328f99211686d7fcc8cc
mobinajafarian <nimobina99@gmail.com>
2023-04-14 02:13:06 +0330

    add ReservationStoreRequest

---
+++ app/Http/Requests/ReservationStoreRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\DateBetween;
+use App\Rules\TimeBetween;
+use Illuminate\Foundation\Http\FormRequest;
+use phpDocumentor\Reflection\PseudoTypes\True_;
+
+class ReservationStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'res_date' => ['required', 'date', new DateBetween, new TimeBetween],
+            'tel_number' => ['required'],
+            'table_id' => ['required'],
+            'guest_number' => ['required'],
+        ];
+    }
+}
