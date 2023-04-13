8013f3c6e9d6fd194d51d1ddd6619b9cdc7ce038
mobinajafarian <nimobina99@gmail.com>
2023-04-14 01:42:43 +0330

    complate tableController

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TableStoreRequest;
 use App\Models\Table;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class TableController extends Controller
     public function index()
     {
         $tables = Table::all();
-        return view('admin.tables.index' , compact('tables'));
+        return view('admin.tables.index', compact('tables'));
     }
 
     /**
@@ -28,9 +29,16 @@ class TableController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TableStoreRequest $request)
     {
-        //
+        Table::create([
+            'name' => $request->name,
+            'guest_number' => $request->guest_number,
+            'status' => $request->status,
+            'location' => $request->location,
+        ]);
+
+        return to_route('admin.tables.index')->with('success', 'Table created successfully.');
     }
 
     /**
@@ -44,24 +52,31 @@ class TableController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Table $table)
     {
-        //
+        return view('admin.tables.edit', compact('table'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TableStoreRequest $request, Table $table)
     {
-        //
+        $table->update($request->validated());
+
+        return to_route('admin.tables.index')->with('success', 'Table updated successfully.');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Table $table)
     {
-        //
+        $table->reservations()->delete();
+        $table->delete();
+        
+        return to_route('admin.tables.index')->with('success', 'Table updated successfully.');
+
     }
 }
