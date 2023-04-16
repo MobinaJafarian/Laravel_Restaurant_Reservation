333c403d42a727ba23f01f700cf031b87f8f9ef0
mobinajafarian <nimobina99@gmail.com>
2023-04-17 02:47:00 +0330

    add DateBetween rule

---
+++ app/Rules/DateBetween.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Rules;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class DateBetween implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $pickupDate = Carbon::parse($value);
+        $lastDate = Carbon::now()->addWeek();
+
+        if (!$value >= now() && $value <= $lastDate) {
+            $fail('Please choose the date between a week from now.');
+        }
+    }
+
+}
