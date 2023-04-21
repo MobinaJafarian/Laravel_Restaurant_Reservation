974104babbc1cd7f532ddfb853885191b495b0f2
mobinajafarian <nimobina99@gmail.com>
2023-04-22 01:14:03 +0330

    add frontend routes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -47,11 +47,11 @@
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
                         href="/">Home</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                        {{-- href="{{ route('categories.index') }}">Categories</a> --}}
+                        href="{{ route('categories.index') }}">Categories</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                        {{-- href="{{ route('menus.index') }}">Our Menu</a> --}}
+                        href="{{ route('menus.index') }}">Our Menu</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                        {{-- href="{{ route('reservations.step.one') }}">Make Reservation</a> --}}
+                        href="{{ route('reservations.step-one') }}">Make Reservation</a>
     
                 </div>
             </nav>
