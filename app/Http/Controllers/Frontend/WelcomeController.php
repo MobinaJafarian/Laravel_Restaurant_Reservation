5c755bc94dfe912be178c2bad24e2c70912dbc01
mobinajafarian <nimobina99@gmail.com>
2023-04-24 16:39:18 +0330

    add welcom controller

---
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        $specials = Category::where('name', 'specials')->first();
+        return view('welcome', compact('specials'));
+    }
+    public function thankyou()
+    {
+        return view('thankyou');
+    }
+}
